1919b841e42f4c7d18390c2aa38e14f65ea8f9a0
Datiel124 <crazybooze12@gmail.com>
2022-07-20 22:21:11 -0400

    Added music player (Only plays random music)

---
+++ Scenes/entities/MusicPlayer/MusicPlayer.gd
@@ -0,0 +1,51 @@
+extends Node
+
+var custom_songs_enabled = true
+var is_playing = false
+var song_array_id = 0
+var file_name
+var file_extension
+var file_path
+
+var supported_types = ["mp3", "wav", "ogg"]
+
+var randomize_playlist = true
+onready var AudioPlayer = $AudioPlayer
+
+
+func _ready():
+	if randomize_playlist == true:
+		randomize_and_play()
+	pass
+
+func _process(delta):
+	if Input.is_action_pressed("refresh_songs"):
+		print("Refreshing music list..")
+		UserConfigs.loadmusic()
+		randomize_and_play()
+		
+	if custom_songs_enabled:
+		pass
+
+func playsong(index):
+	var song = File.new()
+	file_name = UserConfigs.songs[index].get_file()
+	file_extension = UserConfigs.songs[index].get_extension()
+	file_path = UserConfigs.songs[index]
+	print("Now playing, " + file_name)
+	
+	if file_extension == "mp3":
+		song.open(file_path, File.READ)
+		var bytes = song.get_buffer(song.get_len())
+		var audio_stream = AudioStreamMP3.new()
+		audio_stream.data = bytes
+		AudioPlayer.set_stream(audio_stream)
+		AudioPlayer.play()
+		song.close()
+		pass
+
+func randomize_and_play():
+		randomize()
+		var random_song_index:int = randi() % UserConfigs.songs.size()
+		print("Got music index " + str(random_song_index))
+		playsong(random_song_index)
--- scripts/globals/UserConfigs.gd
+++ scripts/globals/UserConfigs.gd
@@ -2,12 +2,14 @@ extends Node
 
 var aim_sens : float = 0.13
 var aim_ADS_mult : float = 0.8
+var songs = []
 
 var username : String = "player"
 
 func _ready() -> void:
 	loadconfigs()
-
+	loadmusic()
+	
 func _notification(what: int) -> void:
 	if what == MainLoop.NOTIFICATION_WM_QUIT_REQUEST:
 		saveconfigs()
@@ -36,3 +38,43 @@ func loadconfigs():
 		for i in data.keys():
 			set(i, data[i])
 	emit_signal('loaded_all_configs')
+
+func add_music(dir : Directory, files:Array, directories: Array):
+	print("Scanning for music..")
+	var file_name = dir.get_next()
+	while (file_name != ""):
+		var file_path = dir.get_current_dir() + "/" + file_name
+		if dir.current_is_dir():
+			print("Found Directory %s. Ignoring." % file_path)
+			#emit_signal("found_directory")
+		else:
+			files.append(file_path)
+			print("Found file %s, Adding to array." % file_path)
+			#emit_signal("found_song")
+		file_name = dir.get_next()
+		
+	dir.list_dir_end()
+
+func loadmusic():
+	#Define directory variable
+	var directory = Directory.new()
+	#Open user folder
+	directory.open("user://")
+	#If music directory doesn't exist, create it
+	if not directory.dir_exists("user://music"):
+		print("Couldn't find the music folder, creating..")
+		directory.make_dir("music")
+	#If it does exist, get all files in it and add it to an array
+	else:
+		if directory.open("user://music") == OK:
+			print("Found Music Folder")
+			var directories = []
+			songs.clear()
+			directory.list_dir_begin()
+			add_music(directory, songs, directories)
+			print("Added all the songs!")
+			#emit_signal("got_songs")
+		else:
+			print("Couldn't open the music folder.. Must've not been made!")
+
+
