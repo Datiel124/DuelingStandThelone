74713e8c6393b4156391a3d912aca0e8e7048cd1
toafloast <toafloast@gmail.com>
2022-07-22 11:13:05 -0700

    ui work

--- Scenes/entities/Player/Player.gd
+++ Scenes/entities/Player/Player.gd
@@ -2,15 +2,16 @@ extends KinematicBody
 class_name Player
 
 #Physics-related variables
-var air_accel:float 	= 1.0
+var air_accel:float 	= 5.0
 var normal_accel:float 	= 8.0
-var speed:float 		= 10.0
+var friction:float		= 16.0
+var max_ground_speed:float 		= 10.0
+var max_air_speed:float = 2.5
 var direction:Vector3 	= Vector3()
 var gravity:float		= 15.0
 var jump_force:float 	= 8.0
 var full_contact:bool 	= true
 var Velocity:Vector3 	= Vector3()
-var time:float			= 0.0
 
 #health
 var health = 100 setget setHealth
@@ -49,11 +50,11 @@ func _process(delta):
 func _physics_process(delta):
 	direction = Vector3.ZERO
 	FSM._state_logic(delta)
-	time += delta
+	$UI/speeddisplay.text = "h-speed : " + str(Vector2(Velocity.x, Velocity.z).length())
 
 #Tilts camera based on input.
 func view_roll(delta):
-	var rotation_speed : float = speed * delta
+	var rotation_speed : float = max_ground_speed * delta
 	var roll_amount : int = 2
 	var angle : float = roll_amount * (int(Input.is_action_pressed("MoveRight")) + -int(Input.is_action_pressed("MoveLeft")))
 	Head.rotation.z = lerp(Head.rotation.z, -deg2rad(angle), rotation_speed)
@@ -82,18 +83,32 @@ func calc_direction(delta):
 #Applies the physics- just does move_and_slide.
 func apply_physics(delta):
 	#Applies physics. Typically called last.
-	#Get the horizontal component of movement.
-	var h_accel = normal_accel if check_ground() > 0b00 else air_accel
-	var horizontal = Velocity.linear_interpolate(direction * speed, h_accel * delta)
+	#Apply friction first.
+	
+	var horizontal = Vector3.ZERO
+	if check_ground() > 0b00:
+		#On ground. Apply friction
+		Velocity = Velocity.move_toward(Vector3.ZERO, friction * delta)
+		
+		var cur_speed = Vector2(Velocity.x,Velocity.z).dot(Vector2(direction.x, direction.z))
+		var add_speed = clamp(max_ground_speed - cur_speed, 0, max_ground_speed)
+		horizontal = lerp(Velocity, Velocity + (add_speed * direction), normal_accel * delta)
+	else:
+		#Player is airborne. Do air physics.
+		var cur_speed = Vector2(Velocity.x,Velocity.z).dot(Vector2(direction.x, direction.z))
+		var add_speed = clamp(max_air_speed - cur_speed, 0, max_ground_speed)
+		horizontal = lerp(Velocity, Velocity + (add_speed * direction), air_accel * delta)
 	Velocity.z = horizontal.z
 	Velocity.x = horizontal.x
 	
+	#After doing that movement stuff, check for collisions with ground and adjust to match slope
 	move_and_slide(Velocity, Vector3.UP)
 
 #Useful for things that sort of override other effects.
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed('Jump') and full_contact:
 		Velocity.y = jump_force
+	
 	if event.is_action_pressed("fire"):
 		if(currentWeapon): #Check isn't required- but just in case.
 			currentWeapon.shoot()
