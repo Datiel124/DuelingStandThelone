48f85393944ebe6ad45610ce60e34633bf3c0f02
toafloast <toafloast@gmail.com>
2022-09-17 17:20:46 -0700

    updated configs, attempted options menu (to be scrapped and redone)

--- Scenes/ui/options/Options.gd
+++ Scenes/ui/options/Options.gd
@@ -1,6 +1,51 @@
 extends Popup
 
-
 func _on_Options_about_to_show() -> void:
 	$AnimationPlayer.play('popup')
 	pass # Replace with function body.
+
+
+func _ready() -> void:
+	#read audio settings saved and apply them to the sliders in options
+	var opts = [UserConfigs.options_master_volume,
+		UserConfigs.options_game_volume,
+		UserConfigs.options_music_volume,
+		UserConfigs.options_ui_volume]
+	$'%MASTER-audio'.set_value(opts[0])
+	$'%GAME-audio'.set_value(opts[1])
+	$'%MUSIC-audio'.set_value(opts[2])
+	$'%UI-audio'.set_value(opts[3])
+	AudioServer.set_bus_volume_db(0, linear2db(opts[0]))
+	AudioServer.set_bus_volume_db(3, linear2db(opts[1]))
+	AudioServer.set_bus_volume_db(4, linear2db(opts[2]))
+	AudioServer.set_bus_volume_db(5, linear2db(opts[3]))
+	pass
+
+
+func _on_exitbutton_pressed() -> void:
+	$AnimationPlayer.play('close')
+	pass # Replace with function body.
+
+
+func _on_MASTERaudio_value_changed(value: float) -> void:
+	UserConfigs.options_master_volume = value
+	AudioServer.set_bus_volume_db(0, linear2db(value))
+	pass # Replace with function body.
+
+
+func _on_GAMEaudio_value_changed(value: float) -> void:
+	UserConfigs.options_game_volume = value
+	AudioServer.set_bus_volume_db(3, linear2db(value))
+	pass # Replace with function body.
+
+
+func _on_MUSICaudio_value_changed(value: float) -> void:
+	UserConfigs.options_music_volume = value
+	AudioServer.set_bus_volume_db(4, linear2db(value))
+	pass # Replace with function body.
+
+
+func _on_UIaudio_value_changed(value: float) -> void:
+	UserConfigs.options_ui_volume = value
+	AudioServer.set_bus_volume_db(5, linear2db(value))
+	pass # Replace with function body. 
--- scripts/globals/UserConfigs.gd
+++ scripts/globals/UserConfigs.gd
@@ -8,18 +8,31 @@ var is_vsync = false
 var show_fps = false
 var is_view_roll = true
 
+#music saved 0 to 100 (percentage)
+var options_master_volume : float = 1.0
+var options_game_volume : float = 1.0
+var options_music_volume : float = 1.0
+var options_ui_volume : float = 1.0
+
+
 var username : String = "player"
+
 
 func _ready() -> void:
 	loadconfigs()
+
 
 func _notification(what: int) -> void:
 	if what == MainLoop.NOTIFICATION_WM_QUIT_REQUEST:
 		saveconfigs()
 
+
 signal loaded_all_configs
 
+
 func saveconfigs():
+	print("Saving configs...")
+	var start = Time.get_ticks_usec()
 	var save = File.new()
 	save.open("user://configs.save", File.WRITE)
 #	save.store_line(to_json(variables))
@@ -27,9 +40,11 @@ func saveconfigs():
 	var thisScript : GDScript = get_script()
 	for propertyInfo in thisScript.get_script_property_list():
 		properties[propertyInfo.name] = get(propertyInfo.name)
-	print(properties)
+#	print(properties)
 	save.store_line(JSON.print(properties))
 	save.close()
+	print("Finished saving configs in " + str(Time.get_ticks_usec() - start) + "us.")
+
 
 func loadconfigs():
 	var save = File.new()
@@ -40,7 +55,9 @@ func loadconfigs():
 		var data = parse_json(save.get_line())
 		for i in data.keys():
 			set(i, data[i])
+	print("loaded all configs")
 	emit_signal('loaded_all_configs')
+
 
 func add_music(dir : Directory, files:Array, directories: Array):
 	print("Scanning for music..")
@@ -58,16 +75,19 @@ func add_music(dir : Directory, files:Array, directories: Array):
 		
 	dir.list_dir_end()
 
+
 func toggle_fullscreen(toggle):
 	OS.window_fullscreen = toggle
 	UserConfigs.is_fullscreen = toggle
 	UserConfigs.saveconfigs()
+
 
 #Vsync Toggle
 func toggle_vsync(toggle):
 	OS.vsync_enabled = toggle
 	UserConfigs.is_vsync = toggle
 	UserConfigs.saveconfigs()
+
 
 #Show FPS Toggle
 func toggle_showfps(toggle):
